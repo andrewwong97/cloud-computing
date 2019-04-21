2db1742acaa902b18cf169efea7a76c0ad1d49a6
Benjamin Hoertnagl-Pereira <bpereira615@gmail.com>
2019-04-21 16:49:50 -0400

    Allow variable number of chains in testing script

--- mapreduce/test_chained_mapreduce.sh
+++ mapreduce/test_chained_mapreduce.sh
@@ -8,30 +8,60 @@ M=10
 
 shuf -i 0-$N -n $M > test_input.txt
 
+# $1 - db type (test, replicated, cached), $2 - number of chains
 function test {
-	cat test_input.txt | python $1 | python mapper.py | python reducer.py > test_output_1.txt
-	cat test_output_1.txt | python $1 | python mapper.py | python reducer.py > test_output_2.txt
-	cat test_output_2.txt | python $1 | python mapper.py | python reducer.py > test_output_3.txt
-	cat test_output_3.txt | python $1 | python mapper.py | python reducer.py > test_output_4.txt
-	cat test_output_4.txt | python $1 | python mapper.py | python reducer.py > test_output.txt
+	cat test_input.txt | python $1 | python mapper.py | python reducer.py > "0.txt"
+	
+	COUNTER=0
+	echo
+	while [ $((COUNTER + 2)) -lt $2 ]; do
+		cat "$COUNTER.txt" | python $1 | python mapper.py | python reducer.py > "$((COUNTER+1)).txt"
+		rm "$COUNTER.txt"
+		let COUNTER=COUNTER+1 
+	done
 
-	rm test_output_1.txt
-	rm test_output_2.txt
-	rm test_output_3.txt
-	rm test_output_4.txt
-
-	cp test_output.txt "$1_output.txt"
-	rm test_output.txt
+	cat "$COUNTER.txt" | python $1 | python mapper.py | python reducer.py > "$1_$2_output.txt"
+	rm "$COUNTER.txt"
 }
 
+echo "==================== 2 Chain ===================="
 echo "Test DB (in memory Python dict):"
-time (test "test_db.py")
+time (test "test_db.py" 2)
 echo
 
 echo "Replicated DB (sharded across nodes):"
-time (test "shard_db.py")
+time (test "shard_db.py" 2)
 echo
 
 echo "Cached DB (single node with Redis)"
-time (test "cache_db.py")
+time (test "cache_db.py" 2)
+echo
+
+
+echo "==================== 4 Chain ===================="
+echo "Test DB (in memory Python dict):"
+time (test "test_db.py" 4)
+echo
+
+echo "Replicated DB (sharded across nodes):"
+time (test "shard_db.py" 4)
+echo
+
+echo "Cached DB (single node with Redis)"
+time (test "cache_db.py" 4)
+echo
+
+
+
+echo "==================== 8 Chain ===================="
+echo "Test DB (in memory Python dict):"
+time (test "test_db.py" 8)
+echo
+
+echo "Replicated DB (sharded across nodes):"
+time (test "shard_db.py" 8)
+echo
+
+echo "Cached DB (single node with Redis)"
+time (test "cache_db.py" 8)
 echo
