dd27c10378b16f478c997b693135cbf0279d1cb6
Dan <dsohn45@gmail.com>
2019-04-21 18:02:52 -0400

    another bug

--- mapreduce/gen_data/create_data.py
+++ mapreduce/gen_data/create_data.py
@@ -11,12 +11,12 @@ from util import N
 # by running mongoimport --db cloud --collection messages --file /path/to/data.json --jsonArray
 d = []
 init = int(sys.argv[1])
-file = 'data' + sys.argv[1] + '.json'
+#file = 'data' + sys.argv[1] + '.json'
 
 for i in range(init*500000, init+ 500000):
 	message = ''.join(random.choice(string.ascii_uppercase + string.ascii_lowercase + string.digits) for _ in range(100))
 	d.append({ "_id": i, "message": message })
-with open(file, 'w') as f:
+with open('data.json', 'w') as f:
 	json.dump(d, f, indent=4, ensure_ascii=False)
 
-close(file)
+close('data.json')
