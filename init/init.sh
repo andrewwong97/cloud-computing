f61757265cf2943405917e3200a1f04ce3471106
Dan <dsohn45@gmail.com>
2019-05-09 18:39:24 -0400

    final init/install.sh

--- init/init.sh
+++ init/init.sh
@@ -2,9 +2,14 @@
 
 sudo mkdir /data
 sudo mkdir /data/db
-sudo echo '10.142.0.2	hadoop' >> /etc/hosts
-sudo echo '10.128.0.3	router' >> /etc/hosts
-sudo echo '10.128.0.4	config' >> /etc/hosts
-sudo echo '10.128.0.5	shard1' >> /etc/hosts
-sudo echo '10.128.0.6	shard2' >> /etc/hosts
-sudo echo '10.142.0.8	shard3' >> /etc/hosts
+sudo echo '10.128.0.4	config
+10.150.0.2	hadoop
+10.128.0.3	router
+10.142.0.9	shard1
+10.142.0.10	shard2
+10.142.0.8	shard3
+10.142.0.11	shard4
+10.142.0.12	shard5
+10.142.0.13	shard6
+10.142.0.14	shard7
+10.142.0.15	shard8' >> /etc/hosts
