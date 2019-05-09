8b2e20c71297ca6971ec2e6fd642b1c3efa0a82f
Dan <dsohn45@gmail.com>
2019-05-09 18:08:12 -0400

    added install mongo script

---
+++ init/install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+sudo apt-get update && sudo apt-get upgrade
+sudo apt-get install dirmgr -y
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 9DA31620334BD75D9DCB49F368818C72E52529D4
+echo "deb http://repo.mongodb.org/apt/debian stretch/mongodb-org/4.0 main" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.0.list
+sudo apt-get update
+sudo apt-get install -y mongodb-org
+mkdir /data
+echo '10.142.0.2	hadoop' >> /etc/hosts
+echo '10.128.0.3	router' >> /etc/hosts
+echo '10.128.0.5	shard1' >> /etc/hosts
+echo '10.128.0.6	shard2' >> /etc/hosts
+echo '10.128.0.4	config' >> /etc/hosts
