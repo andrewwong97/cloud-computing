907084dc6260ccd093e11a5e29b8ee71aa762a80
Benjamin Hoertnagl-Pereira <bpereira615@gmail.com>
2019-05-10 07:37:51 +0000

    Better memory profiling for db

--- mapreduce/db_profile.sh
+++ mapreduce/db_profile.sh
@@ -2,14 +2,12 @@
 # $1 - output csv file 
 rm -rf $1
 > $1
-echo "TIME_STAMP, CPU" | tee -a $1
+echo "TIME_STAMP, CPU (%), MEM (kB)" | tee -a $1
 
 while :
 do
 	DATE=`date +"%H:%M:%S:%s%:z"`
-
-	# cpu usage
-	echo -n "$DATE, " | tee -a $1
 	CPU=$(top -b -n 1| grep -w mongod | tr -s ' ' | cut -d ' ' -f 10)
-	echo -n "$CPU" | tee -a $1	
+	MEM=$(sudo pmap -x $(pgrep mongo) | tail -1 | grep -o -E '[0-9]+' | head -1)
+	echo "$DATE, $CPU, $MEM" | tee -a $1	
 done
