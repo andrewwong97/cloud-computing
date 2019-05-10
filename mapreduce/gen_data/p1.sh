5aedcbf7e83e5ca9e4af11323d6bee6cb60590e9
Dan <dsohn45@gmail.com>
2019-05-09 21:14:20 -0400

    split pop.sh

---
+++ mapreduce/gen_data/p1.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+mongo router:27017/single << EOF
+use single
+db.messages.drop()
+EOF
+
+for i in {seq 0 133}
+do
+   python create_data.py $i
+   mongoimport --db single --collection messages --file data.json --jsonArray
+done
---
+++ mapreduce/gen_data/p3.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+mongo router:27017/single << EOF
+use single
+db.messages.drop()
+EOF
+
+for i in {seq 268 401}
+do
+   python create_data.py $i
+   mongoimport --db single --collection messages --file data.json --jsonArray
+done
