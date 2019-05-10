879608e7665e31d3247ae1e8656a7079a984a36f
Benjamin Hoertnagl-Pereira <bpereira615@gmail.com>
2019-05-10 15:40:32 -0400

    Profiling for shards

--- mapreduce/db_profile.sh
+++ mapreduce/db_profile.sh
@@ -3,24 +3,26 @@
 CSV_OUTPUT=$1
 
 # resart mongod to track accurate memory usage
-sudo pkill mongod
-sleep 2
-sudo mongod --bind_ip localhost,single --dbpath /data/db --fork --logpath /var/log/mongodb.log
-sleep 2
+#sudo pkill mongod
+#sleep 2
+#sudo mongod --bind_ip localhost,single --dbpath /data/db --fork --logpath /var/log/mongodb.log
+#sleep 2
 
 rm -rf $CSV_OUTPUT
 > $CSV_OUTPUT
-echo "TIME_STAMP, CPU (%), MEM (kB)" | tee -a $CSV_OUTPUT
+echo "TIME_STAMP, CPU_1 (%), CPU_2 (%), MEM (kB)" | tee -a $CSV_OUTPUT
 
 while :
 do
 	DATE=`date +"%H:%M:%S:%N"`
 
-    # top CPU output is inconsistent, hacky fix
-	CPU=$(top -b -n 1| grep -w mongod | tr -s ' ' | cut -d ' ' -f 9)
-    if [[ $CPU != *"."* ]]; then
-        CPU=$(top -b -n 1| grep -w mongod | tr -s ' ' | cut -d ' ' -f 10)
-    fi
+    	# top CPU output is inconsistent, hacky fix
+	CPU_1=$(top -b -n 1| grep -w mongod | head -1 | tr -s ' ' | cut -d ' ' -f 9)
+	CPU_2=$(top -b -n 1| grep -w mongod | tail -1 | tr -s ' ' | cut -d ' ' -f 9)
+	#CPU=$((CPU_1 + CPU_2))
+	#if [[ $CPU != *"."* ]]; then
+       	# 	CPU=$(top -b -n 1| grep -w mongod | tr -s ' ' | cut -d ' ' -f 10)
+    	#fi
 	MEM=$(sudo pmap -x $(pgrep mongo) | tail -1 | grep -o -E '[0-9]+' | head -1)
-	echo "$DATE, $CPU, $MEM" | tee -a $CSV_OUTPUT	
+	echo "$DATE, $CPU_1, $CPU_2, $MEM" | tee -a $CSV_OUTPUT	
 done
