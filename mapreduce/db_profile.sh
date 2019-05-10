c1b0db27a5282854a5f1a5dd0c69e981e79615a7
Benjamin Hoertnagl-Pereira <bpereira615@gmail.com>
2019-05-10 07:06:03 +0000

    Profile CPU only for mongod

--- mapreduce/db_profile.sh
+++ mapreduce/db_profile.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-# $1 - output csv file, $2 - wait between samples (in ms) 
+# $1 - output csv file 
 rm -rf $1
 > $1
-echo "TIME_STAMP, CPU, MEMORY, TOTAL" | tee -a $1
+echo "TIME_STAMP, CPU" | tee -a $1
 
 while :
 do
@@ -11,13 +11,5 @@ do
 	# cpu usage
 	echo -n "$DATE, " | tee -a $1
 	CPU=$(top -b -n 1| grep -w mongod | tr -s ' ' | cut -d ' ' -f 10)
-	echo -n "$CPU, " | tee -a $1
-	
-	# memory usage (percentage)
-	MONGOD_MEM=$(top -b -n 1| grep -w mongod | tr -s ' ' | cut -d ' ' -f 11)
-	TOTAL_MEM=$(free -m | grep Mem | tr -s ' ' | cut -d ' ' -f 2)
-	echo -n "$MONGOD_MEM, " | tee -a $1
-	echo "$TOTAL_MEM" | tee -a $1
-	
-	sleep $2
+	echo -n "$CPU" | tee -a $1	
 done
