fa54216644e46e0eb792cc7cdbcf77f945262dba
Dan <dsohn45@gmail.com>
2019-05-09 23:19:43 -0400

    please

--- mapreduce/gen_data/c3.py
+++ mapreduce/gen_data/c3.py
@@ -3,6 +3,16 @@ import hashlib
 import string
 import json
 import sys
+import os
+
+min_lc = ord(b'a')
+len_lc = 26
+
+def write_random_lowercase(n):
+    ba = bytearray(os.urandom(n))
+    for i, b in enumerate(ba):
+        ba[i] = min_lc + b % len_lc # convert 0..255 to 97..122
+    return ba.decode('utf-8')
 
 # generate random mongodb key (_id field) and message
 # export mongodb dump to stdout
@@ -10,16 +20,10 @@ import sys
 # by running mongoimport --db cloud --collection messages --file /path/to/data.json --jsonArray
 d = []
 print(sys.argv[1])
-init = int(sys.argv[1])*500000
-#file = 'data' + sys.argv[1] + '.json'
-
-choice = string.ascii_uppercase + string.ascii_lowercase + string.digits
+init = int(sys.argv[1]) * 500000
 
 for i in range(init, init + 500000):
-	message = ''.join(random.choice(choice) for _ in range(100))
-	d.append({ "_id": i, "message": message })
-
-
+	d.append({ "_id": i, "message": write_random_lowercase(100) })
 
 with open('data3.json', 'w') as f:
 	json.dump(d, f, indent=4, ensure_ascii=False)
