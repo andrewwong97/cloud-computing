b26d71c18c33b02ecedb3c6144bb76332f02b96e
Benjamin Hoertnagl-Pereira <bpereira615@gmail.com>
2019-05-10 02:21:16 -0400

    Track time in nanoseconds

--- mapreduce/test_chained_mapreduce.sh
+++ mapreduce/test_chained_mapreduce.sh
@@ -1,76 +1,40 @@
 #!/bin/bash
 
 # $1 - size of database
-N=$1
+DB_SIZE=$1
 
 # $2 - starting query set size
-M=$2
+KEY_SIZE=$2
 
-shuf -i 0-$N -n $M > test_input.txt
+# $3 - number of chains
+NUM_CHAINS=$3
+
+shuf -i 0-$DB_SIZE -n $KEY_SIZE > test_input.txt
 
 # $1 - db type (single, cloud), $2 - number of chains
 function test {
 
-	if [ $2 = 1 ]; then
-		cat test_input.txt | python db_client.py $1 | python mapper.py | python reducer.py > "$1_$2_output.txt"
-	else
+    INPUT_FILE=test_input.txt
+    COUNTER=0
+    while [ $COUNTER -lt $2 ]; do
+        START=$(($(date +%s%N)/1000000))
+        cat $INPUT_FILE | python db_client.py $1 | python mapper.py | python reducer.py > "temp_$((COUNTER+1)).txt")
+        END=$(($(date +%s%N)/1000000))
+        
+        echo "$COUNTER, $((END - START))"
+        
+        INPUT_FILE="temp_$((COUNTER+1)).txt"
+        let COUNTER=COUNTER+1 
+    done
 
-		COUNTER=0
-		cat test_input.txt | python db_client.py $1 | python mapper.py | python reducer.py > "$COUNTER.txt"
-		while [ $((COUNTER + 2)) -lt $2 ]; do
-			cat "$COUNTER.txt" | python db_client.py $1 | python mapper.py | python reducer.py > "$((COUNTER+1)).txt"
-			rm "$COUNTER.txt"
-			let COUNTER=COUNTER+1 
-		done
-
-		cat "$COUNTER.txt" | python db_client.py $1 | python mapper.py | python reducer.py > "$1_$2_output.txt"
-		rm "$COUNTER.txt"
-	fi
+    cp "temp_$COUNTER.txt" "$1_output.txt"
+    rm -rf temp_*.txt
 }
 
-echo "==================== 1 Chain ===================="
 DATE=`date +"%H:%M:%S:%s%:z"`
 echo "Replicated: $DATE"
-time (test "cloud" 1)
-echo
+test "cloud" $NUM_CHAINS
 
 DATE=`date +"%H:%M:%S:%s%:z"`
 echo "Single: $DATE"
-time (test "single" 1)
-echo
-
-
-echo "==================== 2 Chain ===================="
-DATE=`date +"%H:%M:%S:%s%:z"`
-echo "Replicated: $DATE"
-time (test "cloud" 2)
-echo
-
-DATE=`date +"%H:%M:%S:%s%:z"`
-echo "Single: $DATE"
-time (test "single" 2)
-echo
-
-
-echo "==================== 4 Chain ===================="
-DATE=`date +"%H:%M:%S:%s%:z"`
-echo "Replicated: $DATE"
-time (test "cloud" 4)
-echo
-
-DATE=`date +"%H:%M:%S:%s%:z"`
-echo "Signle: $DATE"
-time (test "single" 4)
-echo
-
-
-echo "==================== 8 Chain ===================="
-DATE=`date +"%H:%M:%S:%s%:z"`
-echo "Replicated: $DATE"
-time (test "cloud" 8)
-echo
-
-DATE=`date +"%H:%M:%S:%s%:z"`
-echo "Single: $DATE"
-time (test "single" 8)
-echo
+test "single" $NUM_CHAINS
