878bbf06ee4c584572f5cee0bda73e0515c51f74
Dan <dsohn45@gmail.com>
2019-05-09 21:23:56 -0400

    p1 fix

--- mapreduce/gen_data/p1.sh
+++ mapreduce/gen_data/p1.sh
@@ -4,7 +4,7 @@ use single
 db.messages.drop()
 EOF
 
-for i in {seq 0 133}
+for i in $(seq 0 133);
 do
    python create_data.py $i
    mongoimport --db single --collection messages --file data.json --jsonArray
