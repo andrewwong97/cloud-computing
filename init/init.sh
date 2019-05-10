ff1ab268f03eb78fce5e18761bd998a2dfb19646
Dan <dsohn45@gmail.com>
2019-05-10 00:46:25 -0400

    new init

--- init/init.sh
+++ init/init.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 
-sudo mkdir /data
-sudo mkdir /data/db
-sudo echo '10.128.0.4	config
-10.150.0.2	hadoop
-10.128.0.3	router
+
+sudo echo '127.0.0.1	localhost
+::1		localhost ip6-localhost ip6-loopback
+ff02::1		ip6-allnodes
+ff02::2		ip6-allrouters
+
+
+10.128.0.8	single
+10.142.0.18	hadoop1
+10.128.0.9	hadoop2
+10.142.0.9	router
 10.142.0.9	shard1
 10.142.0.10	shard2
 10.142.0.8	shard3
 10.142.0.11	shard4
 10.142.0.12	shard5
-10.142.0.13	shard6
-10.142.0.14	shard7
-10.142.0.15	shard8' >> /etc/hosts
+10.142.0.13	shard6' >> /etc/hosts
