53ffaee088de3fa73ae4659779452c131c98ef5f
Dan <dsohn45@gmail.com>
2019-05-11 21:32:08 -0400

    updated /init

--- init/init.sh
+++ init/init.sh
@@ -8,12 +8,13 @@ ff02::2		ip6-allrouters
 
 
 10.128.0.8	single
-10.142.0.18	hadoop1
-10.128.0.9	hadoop2
-10.142.0.17	router
+10.142.0.9	hadoope1
+10.128.0.10	hadoopc1
+10.142.0.20	router
 10.142.0.9	shard1
 10.142.0.10	shard2
 10.142.0.8	shard3
 10.142.0.11	shard4
 10.142.0.12	shard5
-10.142.0.13	shard6' >> /etc/hosts
+10.142.0.13	shard6 
+10.142.0.8	shard7' >> /etc/hosts
